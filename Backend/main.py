9fc3fb7e8293772f3ad93fb0a2f4462f65baa7db
Moroz-Evgeny <ya.moroz2020@gmail.com>
2025-03-16 21:46:30 +0300

    main ip

--- Backend/main.py
+++ Backend/main.py
@@ -14,4 +14,4 @@ main_api_router.include_router(user_router, prefix="/user")
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="localhost", port=8000, reload=True, access_log=True)
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, access_log=True)
